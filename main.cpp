ebd4deb2ec763e66e9718ee3008bb12d2a37e5f3
Caleb Evans <caleb@calebevans.me>
2017-01-25 13:52:45 -0800

    Add test PCB instance in main driver

--- main.cpp
+++ main.cpp
@@ -2,9 +2,12 @@
  * CS 433 Programming Assignment 1: Priority Queue for Processes
  * Caleb Evans, Jonathan Tapia
  */
+#include "PCB.h"
 using namespace std;
 
 int main() {
+
+	PCB pcb1 = PCB(1, 25);
 
 	return 0;
 
