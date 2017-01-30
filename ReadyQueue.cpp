5e9e3e1400eb97e139fdec3903640f5ade5d6a73
Caleb Evans <caleb@calebevans.me>
2017-01-30 13:38:28 -0800

    Eliminate mixed spaces/tabs (tabs are preferred)

--- ReadyQueue.cpp
+++ ReadyQueue.cpp
@@ -9,104 +9,104 @@ using namespace std;
 // See the header file for comments describing each function's purpose
 
 int ReadyQueue::getParentIndex(int i){
-    return (i-1)/2;
+	return (i-1)/2;
 }
 
 int ReadyQueue::getLeftIndex(int i){
-    return i*2 + 1;
+	return i*2 + 1;
 }
 
 int ReadyQueue::getRightIndex(int i){
-    return i*2+2;
+	return i*2+2;
 }
 
 void ReadyQueue::swapNodes(int a, int b){
-    // Swap only if both indexes are legal
-    if(a < size && b < size){
-        PCB* temp = heap[a];
-        heap[a] = heap[b];
-        heap[b] = temp;
-    }
+	// Swap only if both indexes are legal
+	if(a < size && b < size){
+		PCB* temp = heap[a];
+		heap[a] = heap[b];
+		heap[b] = temp;
+	}
 }
 
 ReadyQueue::ReadyQueue(){
-    size = 0;
+	size = 0;
 }
 
 void ReadyQueue::insertProc(PCB* inserted){
-    if(size >= MAX_PROCESS_COUNT){
-        // If queue is already full, do nothing
-        return;
-    }
+	if(size >= MAX_PROCESS_COUNT){
+		// If queue is already full, do nothing
+		return;
+	}
 
-    heap[size] = inserted;
+	heap[size] = inserted;
 	inserted->markAsReady();
-    int i = size;
-    size++;
+	int i = size;
+	size++;
 
-    // Trickle up the new process
-    while(i > 0){
-        int p = getParentIndex(i);
-        if(heap[i]->getPriority() < heap[p]->getPriority()){
-            swapNodes(i, p);
-            i = p;
-        }
-        else {
-            break;
-        }
-    }
+	// Trickle up the new process
+	while(i > 0){
+		int p = getParentIndex(i);
+		if(heap[i]->getPriority() < heap[p]->getPriority()){
+			swapNodes(i, p);
+			i = p;
+		}
+		else {
+			break;
+		}
+	}
 }
 
 PCB* ReadyQueue::removeHighestProc(){
-    if(size == 0){
-        // If empty, return pointer pointing to nowhere
-        return NULL;
-    }
+	if(size == 0){
+		// If empty, return pointer pointing to nowhere
+		return NULL;
+	}
 
-    PCB* result = heap[0];
+	PCB* result = heap[0];
 
-    // Replace root with bottom rightmost node
-    heap[0] = heap[size-1];
-    size--;
+	// Replace root with bottom rightmost node
+	heap[0] = heap[size-1];
+	size--;
 
-    // The current index of the node that is being trickled down
-    int currentIndex = 0;
+	// The current index of the node that is being trickled down
+	int currentIndex = 0;
 
-    // Trickle down the temporary root to its proper position
-    while(true){
-        int leftChild = getLeftIndex(currentIndex);
-        int rightChild = getRightIndex(currentIndex);
-        int smallest;
+	// Trickle down the temporary root to its proper position
+	while(true){
+		int leftChild = getLeftIndex(currentIndex);
+		int rightChild = getRightIndex(currentIndex);
+		int smallest;
 
-        // Find the smallest of the two child nodes
-        if(leftChild < size && heap[currentIndex]->getPriority() > heap[leftChild]->getPriority()){
-            smallest = leftChild;
-        }
-        else{
-            smallest = currentIndex;
-        }
-        if(rightChild < size && heap[smallest]->getPriority() > heap[rightChild]->getPriority()){
-            smallest = rightChild;
-        }
+		// Find the smallest of the two child nodes
+		if(leftChild < size && heap[currentIndex]->getPriority() > heap[leftChild]->getPriority()){
+			smallest = leftChild;
+		}
+		else{
+			smallest = currentIndex;
+		}
+		if(rightChild < size && heap[smallest]->getPriority() > heap[rightChild]->getPriority()){
+			smallest = rightChild;
+		}
 
-        // Swap the current node with the lowest-priority child
-        if(smallest != currentIndex){
-            swapNodes(currentIndex, smallest);
-            currentIndex = smallest;
-        }
-        else{
-            // Otherwise the smallest was the current index, or left and right
-            // children don't exist, so sifting is done
-            break;
-        }
-    }
+		// Swap the current node with the lowest-priority child
+		if(smallest != currentIndex){
+			swapNodes(currentIndex, smallest);
+			currentIndex = smallest;
+		}
+		else{
+			// Otherwise the smallest was the current index, or left and right
+			// children don't exist, so sifting is done
+			break;
+		}
+	}
 
 	result->markAsRunning();
-    return result;
+	return result;
 }
 
 void ReadyQueue::displayQueue(){
-    for(int i = 0; i < size; i++){
-        cout <<"Process " << heap[i]->getID() << " Priority " << heap[i]->getPriority() << endl;
-    }
+	for(int i = 0; i < size; i++){
+		cout <<"Process " << heap[i]->getID() << " Priority " << heap[i]->getPriority() << endl;
+	}
 }
--- ReadyQueue.h
+++ ReadyQueue.h
@@ -6,34 +6,34 @@
 using namespace std;
 
 class ReadyQueue {
-    protected:
+	protected:
 
-    //Maximum number of processes to hold
-    static const int MAX_PROCESS_COUNT = 20;
+	//Maximum number of processes to hold
+	static const int MAX_PROCESS_COUNT = 20;
 
-    //Minimum heap of all ready processes
-    PCB* heap[MAX_PROCESS_COUNT];
+	//Minimum heap of all ready processes
+	PCB* heap[MAX_PROCESS_COUNT];
 
-    //Current number of processes in the heap
-    int size;
+	//Current number of processes in the heap
+	int size;
 
 	// Retrieve the index of node i's parent
-    int getParentIndex(int i);
-    // Retrieve the index of node i's left child
+	int getParentIndex(int i);
+	// Retrieve the index of node i's left child
 	int getLeftIndex(int i);
 	// Retrieve the index of node i's right child
-    int getRightIndex(int i);
+	int getRightIndex(int i);
 
 	// Swap the nodes at the given indices
-    void swapNodes(int a, int b);
+	void swapNodes(int a, int b);
 
-    public:
-    ReadyQueue();
+	public:
+	ReadyQueue();
 
 	// Insert the given PCB instance into the queue
-    void insertProc(PCB* inserted);
+	void insertProc(PCB* inserted);
 	// Remove the highest priority process from the queue
-    PCB* removeHighestProc();
+	PCB* removeHighestProc();
 	// Display each node in the queue, one node per line, in array-order
-    void displayQueue();
+	void displayQueue();
 };
